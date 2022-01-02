0bcd6956f906b0be13d5ec82285616090c77a697
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-02 15:17:06 -0800

    wip: context menu

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,9 +1,10 @@
+import ReactDOMServer from "react-dom/server"
 import * as ContextMenu from "@radix-ui/react-context-menu"
 import { CodeIcon, MagicWandIcon, GitHubLogoIcon } from "@radix-ui/react-icons"
 
-export default function Card({ source }: ISnippet): JSX.Element {
+export default function Card({ source, animation }: ISnippet): JSX.Element {
   const itemStyle =
-    "flex items-center px-2 py-2 text-xs rounded-md outline-none cursor-default select-none text-gray-500 focus:bg-white/5 transition-colors duration-200"
+    "flex items-center px-2 py-2 text-xs rounded-md outline-none cursor-default select-none text-gray-500 focus:bg-white/5 transition-colors duration-200 disabled:opacity-75"
   const itemTextStyle = "ml-2 flex-grow text-gray-200"
   return (
     <ContextMenu.Root>
@@ -13,12 +14,18 @@ export default function Card({ source }: ISnippet): JSX.Element {
         </div>
       </ContextMenu.Trigger>
       <ContextMenu.Content className="radix-state-open:animate-fade-in w-52 px-1.5 py-1.5 rounded-lg bg-gray-900/95 backdrop-blur ring-1 ring-inset ring-gray-500/30">
-        <ContextMenu.Item className={itemStyle}>
+        <ContextMenu.Item
+          className={itemStyle}
+          onClick={() => navigator.clipboard.writeText(ReactDOMServer.renderToString(source))}
+        >
           <CodeIcon className="mb-px" />
           <span className={itemTextStyle}>Copy Source</span>
           <kbd>S</kbd>
         </ContextMenu.Item>
-        <ContextMenu.Item className={itemStyle}>
+        <ContextMenu.Item
+          className={itemStyle}
+          onClick={() => navigator.clipboard.writeText(animation || "no custom animations used")}
+        >
           <MagicWandIcon className="mb-[2px]" />
           <span className={itemTextStyle}>Copy Keyframes</span>
           <kbd>K</kbd>

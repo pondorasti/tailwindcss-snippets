349126e729507458c40d754cc3c976cb9ec538c0
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-01 20:24:09 -0800

    remove: template code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />

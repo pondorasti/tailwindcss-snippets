3f3b0148c079c1bf9a67a2468b0274235d83c32c
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-02 15:46:35 -0800

    fix: safari overflow bug

--- data/snippets.tsx
+++ data/snippets.tsx
@@ -40,7 +40,7 @@ const snippets: ISnippet[] = [
     title: "Underlay Left",
     editLink: "https://github.com/Pondorasti/tailwindcss-snippets/blob/main/data/snippets.tsx#L40",
     source: (
-      <div className="group px-6 py-3 rounded-lg bg-gray-200 overflow-hidden relative before:absolute before:bg-sky-600 before:bottom-0 before:left-0 before:h-full before:w-full before:-translate-x-full hover:before:translate-x-0 before:transition before:ease-in-out before:duration-500">
+      <div className="group [transform:translateZ(0)] px-6 py-3 rounded-lg bg-gray-200 overflow-hidden relative before:absolute before:bg-sky-600 before:bottom-0 before:left-0 before:h-full before:w-full before:-translate-x-full hover:before:translate-x-0 before:transition before:ease-in-out before:duration-500">
         <span className="relative z-0 text-black group-hover:text-gray-200 transition ease-in-out duration-500">
           Hover over me
         </span>
@@ -105,7 +105,7 @@ const snippets: ISnippet[] = [
     title: "Underlay Left Right",
     editLink: "https://github.com/Pondorasti/tailwindcss-snippets/blob/main/data/snippets.tsx#L105",
     source: (
-      <div className="group px-6 py-3 rounded-lg bg-gray-200 overflow-hidden relative before:absolute before:bg-sky-600 before:bottom-0 before:left-0 before:h-full before:w-full before:origin-[100%_100%] before:scale-x-0 hover:before:origin-[0_0] hover:before:scale-x-100 before:transition before:ease-in-out before:duration-500">
+      <div className="group [transform:translateZ(0)] px-6 py-3 rounded-lg bg-gray-200 overflow-hidden relative before:absolute before:bg-sky-600 before:bottom-0 before:left-0 before:h-full before:w-full before:origin-[100%_100%] before:scale-x-0 hover:before:origin-[0_0] hover:before:scale-x-100 before:transition before:ease-in-out before:duration-500">
         <span className="relative z-0 text-black group-hover:text-gray-200 transition ease-in-out duration-500">
           Hover over me
         </span>
@@ -141,7 +141,7 @@ const snippets: ISnippet[] = [
     title: "Underlay Expanding Circle",
     editLink: "https://github.com/Pondorasti/tailwindcss-snippets/blob/main/data/snippets.tsx#L141",
     source: (
-      <div className="group px-6 py-3 rounded-lg overflow-hidden bg-gray-200 relative before:absolute before:bg-sky-600 before:top-1/2 before:left-1/2 before:h-8 before:w-8 before:-translate-y-1/2 before:-translate-x-1/2 before:rounded-full before:scale-[0] before:opacity-0 hover:before:scale-[6] hover:before:opacity-100 before:transition before:ease-in-out before:duration-500">
+      <div className="group [transform:translateZ(0)] px-6 py-3 rounded-lg overflow-hidden bg-gray-200 relative before:absolute before:bg-sky-600 before:top-1/2 before:left-1/2 before:h-8 before:w-8 before:-translate-y-1/2 before:-translate-x-1/2 before:rounded-full before:scale-[0] before:opacity-0 hover:before:scale-[6] hover:before:opacity-100 before:transition before:ease-in-out before:duration-500">
         <span className="relative z-0 text-black group-hover:text-gray-200 transition ease-in-out duration-500">
           Hover over me
         </span>

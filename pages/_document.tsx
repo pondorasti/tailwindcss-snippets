765d945998d134760087047fdef94c682b2be7ea
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-02 22:09:37 -0800

    add: meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,39 @@ import NextDocument, { Html, Head, Main, NextScript } from "next/document"
 
 export default class Document extends NextDocument {
   render(): JSX.Element {
+    const title = "TailwindCSS Snippets"
+    const description = "A collection of animation snippets made with TailwindCSS"
+    const image = "https://snippets.alexandru.so/thumbnail.png"
+
     return (
       <Html lang="en" className="nightwind">
         <Head>
+          <title>{title}</title>
+          <meta name="description" content={description} />
+          <link rel="icon" href="/favicon.ico" />
+
+          {/* Title */}
+          <title>{title}</title>
+          <meta name="og:title" content={title} />
+
+          {/* Description */}
+          <meta name="description" content={description} />
+          <meta name="og:description" content={description} />
+
+          {/* Image */}
+          <meta name="twitter:image" content={image} />
+          <meta name="og:image" content={image} />
+
+          {/* URL */}
+          <meta name="og:url" content="https://snippets.alexandru.so/" />
+
+          {/* General */}
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta httpEquiv="Content-Language" content="en" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@pondorasti" />
+          <meta name="author" content="Alexandru Turcanu" />
+
           {/* Inter Font */}
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head"
 import Card from "@components/Card"
 import LinkPreview from "@components/LinkPreview"
 import snippets from "@data/snippets"
@@ -9,12 +8,6 @@ function Home(): JSX.Element {
 
   return (
     <div className="relative max-w-screen-lg mx-auto px-4 sm:px-6 md:px-8 pb-16">
-      <Head>
-        <title>TailwindCSS Snippets</title>
-        <meta name="description" content="A collection of animation snippets made with TailwindCSS" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <header className="text-white py-8 flex items-center justify-between">
         <div className="tracking-wide font-medium text-lg">TailwindCSS Snippets</div>
         <div className="flex gap-4">
@@ -25,7 +18,12 @@ function Home(): JSX.Element {
               <path d="M22.162 5.656a8.384 8.384 0 0 1-2.402.658A4.196 4.196 0 0 0 21.6 4c-.82.488-1.719.83-2.656 1.015a4.182 4.182 0 0 0-7.126 3.814 11.874 11.874 0 0 1-8.62-4.37 4.168 4.168 0 0 0-.566 2.103c0 1.45.738 2.731 1.86 3.481a4.168 4.168 0 0 1-1.894-.523v.052a4.185 4.185 0 0 0 3.355 4.101 4.21 4.21 0 0 1-1.89.072A4.185 4.185 0 0 0 7.97 16.65a8.394 8.394 0 0 1-6.191 1.732 11.83 11.83 0 0 0 6.41 1.88c7.693 0 11.9-6.373 11.9-11.9 0-.18-.005-.362-.013-.54a8.496 8.496 0 0 0 2.087-2.165z"></path>
             </svg>
           </a>
-          <a className={linkStyle} target="_blank" rel="noopener noreferrer" href="https://github.com/pondorasti/">
+          <a
+            className={linkStyle}
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/pondorasti/tailwindcss-snippets"
+          >
             <span className="sr-only">GitHub account</span>
             <svg className={iconStyle} viewBox="0 0 24 24" fill="currentColor">
               <path

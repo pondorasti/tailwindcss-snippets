1f59e97502cac7273937d346e96564b352b9f695
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-04 10:59:42 -0800

    fix: snippet duplicate key

--- data/snippets.tsx
+++ data/snippets.tsx
@@ -241,7 +241,7 @@ const snippets: ISnippet[] = [
     },`,
   },
   {
-    title: "Pulse",
+    title: "Background Pulse",
     editLink: "https://github.com/Pondorasti/tailwindcss-snippets/blob/main/data/snippets.tsx#L244",
     playgroundLink: "https://play.tailwindcss.com/Mf77KHXg25",
     source: <div className="animate-pulse bg-gray-900 h-12 w-40 rounded-lg"></div>,

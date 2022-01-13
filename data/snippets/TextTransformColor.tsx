89875b76d811b0732406feb81f2549d9fd346c5d
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-12 20:57:50 -0800

    update: TextTransformColor snippet

--- data/snippets/TextTransformColor.tsx
+++ data/snippets/TextTransformColor.tsx
@@ -1,10 +1,16 @@
 const TextTransformColor: ISnippet = {
   title: "Text Transform & Color",
   editLink: "https://github.com/Pondorasti/tailwindcss-snippets/blob/main/data/snippets/TextTransformColor.tsx",
-  playgroundLink: "https://play.tailwindcss.com/zbRrj0ZCee",
+  playgroundLink: "https://play.tailwindcss.com/YQEPZafzdC",
   source: (
-    <div className="text-gray-200 translate-x-0 hover:text-sky-600 hover:translate-x-1 transition ease-in-out duration-200">
-      Hover over me
+    <div className="group text-gray-200 hover:text-sky-600 transition ease-in-out duration-200">
+      Hover over me{" "}
+      <span
+        aria-hidden="true"
+        className="inline-block translate-x-0 group-hover:translate-x-1 transition-transform ease-in-out duration-200"
+      >
+        →
+      </span>
     </div>
   ),
 }
